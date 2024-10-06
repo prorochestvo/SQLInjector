9524f5f7fccb32c70968b5a830b8851b1d9757ee
Seilbek <SeilbekSkindirov@gmail.com>
2024-10-06 09:52:56 +0500

    review

--- internal/expression/limit_test.go
+++ internal/expression/limit_test.go
@@ -2,19 +2,18 @@ package expression
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 var _ expression = &Limit{}
 
 func TestNewLimitFrom(t *testing.T) {
-
 	tests := []struct {
 		expr     string
 		expected []*Limit
 		err      bool
 	}{
-
 		{"", []*Limit{}, false},
 		{"0", []*Limit{{limit: 0}}, false},
 		{"1", []*Limit{{limit: 1}}, false},
@@ -37,26 +36,34 @@ func TestNewLimitFrom(t *testing.T) {
 			}
 		})
 	}
+
+	t.Run("DefaultBehavior", func(t *testing.T) {
+	})
+	t.Run("NegativeNumber", func(t *testing.T) {
+	})
+	t.Run("UnexpectedSymbols", func(t *testing.T) {
+	})
+	t.Run("NonDecimalBase", func(t *testing.T) {
+	})
 }
 
 func TestNewLimit(t *testing.T) {
-	tests := []struct {
+	testCases := []struct {
 		value    int
 		expected int
 	}{
-
 		{-10, 0},
 		{0, 0},
 		{1, 1},
 		{100, 100},
+		{0xFFFFFF, 0xFFFFFF},
 	}
 
-	for _, tt := range tests {
-		t.Run(fmt.Sprintf("value=%d", tt.value), func(t *testing.T) {
-			result := NewLimit(tt.value)
-			if result.limit != tt.expected {
-				t.Errorf("unexpected limit: got %d, expected %d", result.limit, tt.expected)
-			}
+	for _, testCase := range testCases {
+		t.Run(fmt.Sprintf("TestCases%d", testCase.value), func(t *testing.T) {
+			r := NewLimit(testCase.value)
+			// TODO: REVIEW: could use this approach to test the value in all our tests
+			require.Equal(t, testCase.expected, r.limit)
 		})
 	}
 }
