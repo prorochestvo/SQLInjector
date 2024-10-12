aed773c3504fe98529e0e512356db89ea6eb14de
flo0o3 <gosha.matveyshin@gmail.com>
2024-10-12 11:43:20 +0500

    sql expression: test for limit (#3)

--- internal/expression/limit_test.go
+++ internal/expression/limit_test.go
@@ -1,13 +1,62 @@
 package expression
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 var _ expression = &Limit{}
 
 func TestNewLimitFrom(t *testing.T) {
-	t.Skip("not implemented")
+	t.Run("EmptyString", func(t *testing.T) {
+		result, err := NewLimitFrom("") // This string
+		require.NoError(t, err)
+		require.Len(t, result, 0)
+	})
+	t.Run("PositiveNumber", func(t *testing.T) {
+		result, err := NewLimitFrom("1")
+
+		require.NoError(t, err)
+		require.Len(t, result, 1)
+		require.Equal(t, 1, result[0].limit)
+	})
+	t.Run("NegativeNumber", func(t *testing.T) {
+		_, err := NewLimitFrom("-1")
+		require.Error(t, err)
+	})
+	t.Run("ZeroValue", func(t *testing.T) {
+		result, err := NewLimitFrom("0")
+		require.NoError(t, err)
+		require.Len(t, result, 1)
+		require.Equal(t, 0, result[0].limit)
+	})
+	t.Run("UnexpectedSymbols", func(t *testing.T) {
+		_, err := NewLimitFrom("abc")
+		require.Error(t, err)
+	})
+	t.Run("NonDecimalBase", func(t *testing.T) {
+		_, err := NewLimitFrom("1.5")
+		require.Error(t, err)
+	})
 }
 
 func TestNewLimit(t *testing.T) {
-	t.Skip("not implemented")
+	testCases := []struct {
+		value    int
+		expected int
+	}{
+		{-10, 0},
+		{0, 0},
+		{1, 1},
+		{100, 100},
+		{0xFFFFFF, 0xFFFFFF},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(fmt.Sprintf("TestCases:%d", testCase.value), func(t *testing.T) {
+			r := NewLimit(testCase.value)
+			require.Equal(t, testCase.expected, r.limit)
+		})
+	}
 }
