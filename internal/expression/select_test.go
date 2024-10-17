c99e41ee663cd4336e47579399854a3bb175f27a
flo0o3 <gosha.matveyshin@gmail.com>
2024-10-18 03:04:35 +0500

    Test for select tests

--- internal/expression/select_test.go
+++ internal/expression/select_test.go
@@ -1,13 +1,65 @@
 package expression
 
-import "testing"
+import (
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"reflect"
+	"testing"
+)
 
 var _ expression = &Select{}
 
-func TestNewSelectFrom(t *testing.T) {
-	t.Skip("not implemented")
+func TestNewSelect(t *testing.T) {
+	s := NewSelect("id")
+	expectedColumns := []string{"id"}
+	if !reflect.DeepEqual(s.Select(), expectedColumns) {
+		t.Errorf("Expected %v, got %v", expectedColumns, s.Select())
+	}
+
+	s = NewSelect("id", "name", "age")
+	expectedColumns = []string{"id", "name", "age"}
+	if !reflect.DeepEqual(s.Select(), expectedColumns) {
+		t.Errorf("Expected %v, got %v", expectedColumns, s.Select())
+	}
 }
 
-func TestNewSelect(t *testing.T) {
-	t.Skip("not implemented")
+func TestSelect(t *testing.T) {
+	s := NewSelect("id")
+	expected := []string{"id"}
+	if !reflect.DeepEqual(s.Select(), expected) {
+		t.Errorf("Expected %v, got %v", expected, s.Select())
+	}
+
+	s = NewSelect("id", "name", "age")
+	expected = []string{"id", "name", "age"}
+	if !reflect.DeepEqual(s.Select(), expected) {
+		t.Errorf("Expected %v, got %v", expected, s.Select())
+	}
+}
+
+func TestSelect_QueryMod(t *testing.T) {
+	s := NewSelect("id")
+	expectedQueryMod := []qm.QueryMod{qm.Select("id")}
+	if !reflect.DeepEqual(s.QueryMod(), expectedQueryMod) {
+		t.Errorf("Expected %v, got %v", expectedQueryMod, s.QueryMod())
+	}
+
+	s = NewSelect("id", "name", "age")
+	expectedQueryMod = []qm.QueryMod{qm.Select("id", "name", "age")}
+	if !reflect.DeepEqual(s.QueryMod(), expectedQueryMod) {
+		t.Errorf("Expected %v, got %v", expectedQueryMod, s.QueryMod())
+	}
+}
+
+func TestSelect_ToString(t *testing.T) {
+	s := NewSelect("id")
+	expectedString := "Select id"
+	if s.ToString() != expectedString {
+		t.Errorf("Expected %v, got %v", expectedString, s.ToString())
+	}
+
+	s = NewSelect("id", "name", "age")
+	expectedString = "Select id, name, age"
+	if s.ToString() != expectedString {
+		t.Errorf("Expected %v, got %v", expectedString, s.ToString())
+	}
 }
