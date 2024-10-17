c0aecff7708ce2d683ec123aee0d58e618abcfe7
flo0o3 <gosha.matveyshin@gmail.com>
2024-10-18 03:21:13 +0500

    Test for table tests

--- internal/expression/table_test.go
+++ internal/expression/table_test.go
@@ -2,23 +2,90 @@ package expression
 
 import (
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"reflect"
 	"testing"
 )
 
 func TestExtractTableNameAndColumn(t *testing.T) {
-	t.Skip("not implemented")
+	tests := []struct {
+		column         string
+		expectedTable  string
+		expectedColumn string
+	}{
+		{"users/name", "users", "name"},
+		{"name", "", "name"},
+		{"orders/id", "orders", "id"},
+		{"", "", ""},
+	}
+
+	for _, tt := range tests {
+		table, column := extractTableNameAndColumn(tt.column)
+		if table != tt.expectedTable || column != tt.expectedColumn {
+			t.Errorf("extractTableNameAndColumn(%v) = %v, %v; expected %v, %v", tt.column, table, column, tt.expectedTable, tt.expectedColumn)
+		}
+	}
 }
 
 func TestJoinTableNameAndColumn(t *testing.T) {
-	t.Skip("not implemented")
+	var mods []qm.QueryMod
+
+	column := joinTableNameAndColumn("User", "name", &mods)
+	expectedColumn := "\"users\".\"name\""
+	expectedJoin := qm.InnerJoin("\"users\" ON \"users\".\"id\" = \"user_id\"")
+
+	if column != expectedColumn {
+		t.Errorf("joinTableNameAndColumn(\"User\", \"name\") = %v; expected %v", column, expectedColumn)
+	}
+	if len(mods) != 1 || !reflect.DeepEqual(mods[0], expectedJoin) {
+		t.Errorf("Expected join mod %v, got %v", expectedJoin, mods)
+	}
+
+	column = joinTableNameAndColumn("", "name", nil)
+	expectedColumn = "\"name\""
+
+	if column != expectedColumn {
+		t.Errorf("joinTableNameAndColumn(\"\", \"name\") = %v; expected %v", column, expectedColumn)
+	}
 }
 
 func TestToPluralize(t *testing.T) {
-	t.Skip("not implemented")
+	tests := []struct {
+		word     string
+		expected string
+	}{
+		{"box", "boxes"},
+		{"city", "cities"},
+		{"leaf", "leaves"},
+		{"man", "men"},
+		{"woman", "women"},
+		{"dog", "dogs"},
+	}
+
+	for _, tt := range tests {
+		plural := toPluralize(tt.word)
+		if plural != tt.expected {
+			t.Errorf("toPluralize(%v) = %v; expected %v", tt.word, plural, tt.expected)
+		}
+	}
 }
 
 func TestToSnakeCase(t *testing.T) {
-	t.Skip("not implemented")
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"UserName", "user_name"},
+		{"FirstName", "first_name"},
+		{"userID", "user_i_d"},
+		{"simple", "simple"},
+	}
+
+	for _, tt := range tests {
+		snake := toSnakeCase(tt.input)
+		if snake != tt.expected {
+			t.Errorf("toSnakeCase(%v) = %v; expected %v", tt.input, snake, tt.expected)
+		}
+	}
 }
 
 type expression interface {
